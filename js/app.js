da83b40858ccd261d736829821c8d936331ed312
duffield44 <duffieldbenjamin44@gmail.com>
2015-01-30 15:19:46 +0800

    Give Add Button functionality to append items

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	
+
+	/* Allows Add Button to be used as an submit button for input field */
+	$('.add').click(function () {
+		postItem();			
+	})
+
+	/* Appends value of input field to the bottom of div.list in a p.not-checked */
+	function postItem() {
+		var value = $('.input').val();
+		$('.list').append("<p class='not-checked'><i class='fa fa-check'></i>" + value + "<i class='fa fa-times'></p>");
+	}
+
+});
+
+
+
+
+
+
+	
